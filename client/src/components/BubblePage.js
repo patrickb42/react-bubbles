2734b4627ab4d7c23afad66560a40aecb6b8d772
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-23 12:43:47 -0400

    mvp

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(
+    () => {
+      (async () => {
+        try {
+          const response = await axiosWithAuth().get('http://localhost:5000/api/colors');
+          setColorList(response.data)
+        } catch (error) {
+          console.error(error.response);
+        }
+      })();
+    },
+    []
+  );
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Login = ({ history }) => {
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    (async () => {
+      try {
+        const response = await axiosWithAuth().post('http://localhost:5000/api/login', state);
+        localStorage.setItem('token', response.data.payload);
+        history.push('/bubbles');
+      } catch (error) {
+        console.error(error.response);
+      }
+    })();
+  };
+
+  const handleChange = e => setState({...state, [e.target.name]: e.target.value});
+
+  if (localStorage.getItem('token') !== null) history.push('/bubbles');
+
+  return (<>
+    <h1>Welcome to the Bubble App!</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={state.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={state.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  </>);
 };
 
 export default Login;
